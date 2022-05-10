78461c6db23e92597e547ed76f5150b92952f2ef
Milena Nobre <mnmbrito@hotmail.com>
2022-05-10 14:11:20 -0300

    Conclusão do módulo sobre hooks

--- hooks/src/views/App.jsx
+++ hooks/src/views/App.jsx
@@ -1,19 +1,26 @@
 import './App.css'
-import React from 'react'
+import React, {useState} from 'react'
 import { BrowserRouter } from  'react-router-dom'
 
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
+import DataContext, {data} from "../Data/DataContext";
+import Store from "../Data/Store";
 
 const App = props => {
+    const [state, setState] = useState(data) // não é o melhor cenário
 
     return (
+        <Store>
+            <DataContext.Provider value={{state, setState}}>
                 <div className="App">
                     <BrowserRouter>
                         <Menu />
                         <Content />
                     </BrowserRouter>
                 </div>
+            </DataContext.Provider>
+        </Store>
     )
 }
 
--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, {useReducer} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from "../../components/layout/SectionTitle";
+
+import { initialState, reducer } from  '../../store/index'
+import { numberAdd2, login } from "../../store/actions";
 
 const UseReducer = (props) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
     return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
             />
+            <SectionTitle title="STATE NUMBER"/>
+            <div className="center">
+                <span className="text">{ state.number }</span>
+            </div>
+
+            <SectionTitle title="Exercício #01"/>
+            <div className="center">
+                { state.user ?
+                    <span className="text">{state.user.name}</span> :
+                    <span className="text">Sem usuário</span>
+
+                }
+                <div>
+                    <button className="btn" onClick={() => login(dispatch, 'Milena')}> Login </button>
+                    <button className="btn" onClick={() => numberAdd2(dispatch) }>+2</button>
+                </div>
+            </div>
+            <SectionTitle title="Desafio #01" />
+            <div className="center">
+                <div>
+                    <button className="btn" onClick={() => dispatch({type: 'numberMulti7'})}>*7</button>
+                    <button className="btn" onClick={() => dispatch({type: 'numberDiv25'})}>/25</button>
+                    <button className="btn" onClick={() => dispatch({type: 'numberInt'})}>INT</button>
+                    <button className="btn" onClick={() => dispatch({type: 'numberAdd', payload: 100})}>ADD</button>
+                </div>
+
+            </div>
+
         </div>
     )
 }
